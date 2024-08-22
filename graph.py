d2b9b9ecef3e51a8fab2dc20c0c22f2e7806b4cf
Sagiv.M <sgmelamed@gmail.com>
2024-08-22 17:27:34 +0300

    add csv usage

--- graph.py
+++ graph.py
@@ -3,6 +3,10 @@ import random
 from networkx import Graph
 import numpy as np
 import heapq
+import pandas as pd
+
+import statistics
+
 
 class Graph(Graph):
 
@@ -24,12 +28,12 @@ class Graph(Graph):
 
         for u, v in possible_edges[:num_edges]:
             g.add_edge(u, v)
-            g.weights[u,v] = g.get_weight(u, v)
+            g.weights[u, v] = g.get_weight(u, v)
 
         return g
 
-    def get_weight(self,a, b):
-        return abs(a - b + random.randint(-2, 2))
+    def get_weight(self, a, b):
+        return abs(a - b + random.randint(-4, 4))
 
     def compute_page_rank_values(self) -> dict:
         return nx.pagerank(self, 0.85)
@@ -50,19 +54,17 @@ class Heuristic():
     def set_page_rank(self, _page_rank: dict):
         self.page_rank = _page_rank
 
-    def noHeuristic(self,node_a: int, node_b: int):
+    def noHeuristic(self, node_a: int, node_b: int):
         return 0
 
-
-    def computeSimpleHeuristic(self,node_a: int, node_b: int):
+    def computeSimpleHeuristic(self, node_a: int, node_b: int):
         h_value = self.h_values_map[node_a, node_b]
         return h_value
 
-    def computeAdvancedHeuristic(self,node_a: int, node_b: int):
+    def computeAdvancedHeuristic(self, node_a: int, node_b: int):
         h_value = self.h_values_map[node_a, node_b]
         pr_value = self.page_rank[node_b]
         return h_value * pr_value
-
 
 
 def heuristic(node, goals, heuristic_function):
@@ -70,15 +72,16 @@ def heuristic(node, goals, heuristic_function):
     return min(heuristic_function(node, goal) for goal in goals)
 
 
-def update_goals(node: int, goals: list, visited_goals:list):
+def update_goals(node: int, goals: list, visited_goals: list):
     if node in goals:
         visited_goals.append(node)
-        return list(set(sorted(visited_goals))) == sorted(goals)
+        return set(visited_goals) == set(goals)
     return False
+
 
 def a_star(graph, start, goals, heuristic_function):
     open_list = []
-    heapq.heappush(open_list, (0,start))
+    heapq.heappush(open_list, (0, start))
     came_from = {}
     g_score = {start: 0}
     f_score = {start: heuristic(start, goals, heuristic_function)}
@@ -87,39 +90,76 @@ def a_star(graph, start, goals, heuristic_function):
     while open_list:
         size_of_open_list.append(len(open_list))
         current = heapq.heappop(open_list)[1]
-        if update_goals(current,goals,visited_goals):
-            return size_of_open_list
+        if update_goals(current, goals, visited_goals):
+            return True, g_score, size_of_open_list
         for neighbor in graph.neighbors(current):
-            tentative_g_score = g_score[current] + g.weights[current,neighbor]
+            tentative_g_score = g_score[current] + graph.weights[current, neighbor]
 
             if neighbor not in g_score or tentative_g_score < g_score[neighbor]:
-                if update_goals(neighbor,goals,visited_goals):
-                    return size_of_open_list
+                # if update_goals(neighbor, goals, visited_goals):
+                #     return True, g_score, size_of_open_list
                 came_from[neighbor] = current
                 g_score[neighbor] = tentative_g_score
-                f_score[neighbor] = g_score[neighbor] + heuristic(neighbor, goals,heuristic_function)
+                f_score[neighbor] = g_score[neighbor] + heuristic(neighbor, goals, heuristic_function)
                 if neighbor not in [i[1] for i in open_list]:
                     heapq.heappush(open_list, (f_score[neighbor], neighbor))
 
-    return size_of_open_list
+    return False, g_score, size_of_open_list
 
 
+def make_report(finish, g_score, open_list_sizes):
+    return {
+        "finish": finish,
+        "g_score": g_score,
+        "open_list_sizes": open_list_sizes,
+        # "score": sum[g_score[key] for key in goals]
 
-if __name__ == "__main__":
+    }
+
+
+def run_graph(coverage, number_of_targets):
     n_vertices = 100
-    g = Graph.generateGraph(n_vertices, .6)
+    g = Graph.generateGraph(n_vertices, coverage)
     heuristicObj = Heuristic(n_vertices)
     heuristicObj.set_page_rank(g.compute_page_rank_values())
-    print(g.edges)
 
     # Start node and goal nodes
     start = 0
-    goals = [20,40, 60]  # Multiple goals
+    vertices = list(range(0, n_vertices))
+    random.shuffle(vertices)
+    goals = vertices[:number_of_targets]  # Multiple goals
 
     # Perform A* and track the size of the open list
-    open_list_sizes = a_star(g, start, goals, heuristicObj.noHeuristic)
-    print("Sizes of the (NH) open list at each step:", open_list_sizes)
-    open_list_sizes = a_star(g, start, goals, heuristicObj.computeSimpleHeuristic)
-    print("Sizes of the (SH) open list at each step:", open_list_sizes)
-    open_list_sizes = a_star(g, start, goals, heuristicObj.computeAdvancedHeuristic)
-    print("Sizes of the (AH) open list at each step:", open_list_sizes)
+    NH_report = make_report(*a_star(g, start, goals, heuristicObj.noHeuristic))
+    BH_report = make_report(*a_star(g, start, goals, heuristicObj.computeSimpleHeuristic))
+    PRH_report = make_report(*a_star(g, start, goals, heuristicObj.computeAdvancedHeuristic))
+
+    return [
+        ("NH", NH_report),
+        ("BH", BH_report),
+        ("PRH", PRH_report)
+    ]
+
+
+if __name__ == "__main__":
+    coverages = np.array(range(10, 85, 5)) / 100
+    number_of_targets_list = np.array([1,2,4,6,8])
+    reports = list()
+    for coverage in coverages:
+        for number_of_targets in number_of_targets_list:
+            reports.append(((coverage, number_of_targets), run_graph(coverage, number_of_targets)))
+
+    # construct reuslts
+    result = np.ndarray((len(coverages), len(number_of_targets_list), 3))
+    for report in reports:
+        (row, column), settings_report = report
+        row_i = np.where(coverages == row)[0][0]
+        column_i = np.where(number_of_targets_list == column)[0][0]
+        for i, (_, execution) in enumerate(settings_report):
+            result[row_i, column_i, i] = statistics.median(execution["open_list_sizes"])
+        x = 0
+
+    algo = ["NH", "BH", "PRH"]
+    for i in range(result.shape[-2]):
+        df=pd.DataFrame(result[:, i, :], index=coverages.tolist(), columns=algo)
+        df.to_csv(f"{number_of_targets_list[i]}.csv",index=True)
